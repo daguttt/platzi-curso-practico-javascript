e0ca5eba5a9757549b5f05f83fb737ac8f26d7a9
dagutx-laptop <dagutmu667@gmail.com>
2021-08-13 16:24:06 -0500

    Add coupon feature (not terminated)

--- taller2/js/descuento.js
+++ taller2/js/descuento.js
@@ -1,13 +1,20 @@
 const results = document.getElementsByClassName('card__result');
 const price = document.getElementById('price');
 const discount = document.getElementById('discount');
-function getPriceWithDiscount(price, discount) {
-    return (price * (100 - discount)) / 100
+const coupon = document.getElementById('coupons');
+function getPriceWithDiscount(price, discount, coupon = 0) {
+    if (coupon.value == 'no-coupon') {
+        coupon = 0;
+    }
+    return (price * (100 - discount - coupon)) / 100
 }
 const getPriceButton = document.getElementById('get-price-button');
 getPriceButton.addEventListener('click', () => {
-    if (price.value && discount.value) {
-        const priceWithDiscount = getPriceWithDiscount(price.value, discount.value);
+    let price = price.value;
+    let discount = discount.value;
+    let coupon = coupon.value == 'no-coupon' ? 0 : coupon.value;
+    if (price && discount) {
+        const priceWithDiscount = getPriceWithDiscount(price, discount, coupon);
         return results[0].innerHTML = `El precio con descuento es: ${priceWithDiscount}$`;
     }
     return results[0].innerHTML = `Por favor ingresa todos los valores`;
