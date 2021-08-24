ff4538d36248013543c90e65dc45abb0114dee4e
dagut <dagutmu667@gmail.com>
2021-08-23 22:20:04 -0500

    Integrate statical functions with html

--- taller3/js/mediaMedianaModa.js
+++ taller3/js/mediaMedianaModa.js
@@ -1,13 +1,12 @@
 const results = document.getElementsByClassName("card__result");
 const operation = document.getElementById("statistical-operation");
 const calculateButton = document.getElementById("calculate-button");
-const NUMBERS = document.getElementById('numbers');
+const NUMBERS = document.getElementById("numbers");
 const numbers = [1, 1, 2, 3, 2, 3, 3, 4, 5, 4, 4, 3, 4, 2];
 function getMedia(numbers) {
-    let sum = 0;
-    for (number of numbers) {
-        sum += number;
-    }
+    let sum = numbers.reduce((acc, cur) => {
+        return cur + acc;
+    }, 0);
     return sum / numbers.length;
 }
 function getMedian(numbers) {
@@ -35,24 +34,42 @@ function getMode(numbers) {
     const pairs = Object.entries(numbersInventory);
     const [mode, reps] = pairs.reduce((acc, currentValue, i) => {
         if (!acc[1]) return (acc = [[pairs[i][0]], pairs[i][1]]);
+        if (acc[1] < pairs[i][1]) return (acc = [[pairs[i][0]], pairs[i][1]]);
         if (
             acc[1] === pairs[i][1] &&
             acc[0].every((item) => item !== pairs[i][0])
-        )
+        ) {
             acc[0].push(pairs[i][0]);
-        if (acc[1] < pairs[i][1]) return (acc = [[pairs[i][0]], pairs[i][1]]);
+        }
         return acc;
     }, []);
     return { reps, mode };
 }
-function clearInputNumbers(numbers) {
-    numbers = numbers.split(',')
-    numbers = numbers.split(' ')
-    numbers
+function clearInputNumbers(numbersString) {
+    let numbersClear = numbersString.replace(/\W/g, "s");
+    let arrayNumbers = numbersClear.split("s").reduce((acc, cur) => {
+        if (cur) acc.push(Number(cur));
+        return acc;
+    }, []);
+    return arrayNumbers;
 }
 calculateButton.addEventListener("click", () => {
-    let operationValue = operation.value;
+    let operationValue = operation.value,
+        numbers = clearInputNumbers(NUMBERS.value),
+        result;
+
     if (operationValue == "media") {
-        getMedia
+        result = getMedia(numbers);
+        results[0].innerText = `La media de los números ingresados es ${result}.`;
+    }
+    if (operationValue == "mediana") {
+        result = getMedian(numbers);
+        results[0].innerText = `La mediana de los números ingresados es ${result}.`;
+    }
+    if (operationValue == "moda") {
+        result = getMode(numbers);
+        results[0].innerText = `La moda es ${result.mode} con ${result.reps} ${
+            result.reps === 1 ? "repetición" : "repeticiones"
+        }.`;
     }
 });
